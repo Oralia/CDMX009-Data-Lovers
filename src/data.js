83f99cdf4e55153c066a17af214b59139f849eef
abigail-sanlo <abisl9160@gmail.com>
2020-03-03 13:16:34 -0600

    implementando data

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
-// import data from './data/steam/worldbank.js';
-
+import data from './data/worldbank/worldbank.js';
 // esta es una función de ejemplo
-
 export const example = () => {
   return 'example';
 };
+console.log(data);
+//para que oralia pueda progresar
